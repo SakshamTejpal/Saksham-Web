b85ec845597a95a4a08fb91b5d1c7a797dff73fe
saksham-tejpal <saksham.tejpal@gmail.com>
2025-06-17 22:41:35 -0400

    DeploymentFix-About

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import About from './components/about';
+import About from './components/About';
 import Contact from './components/Contact';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
